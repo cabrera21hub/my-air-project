8f93cfa49b3a31fb6b399245bd244fd48830b879
Alan Cabrera <alanc9961693@gmai.com>
2024-05-29 09:39:59 -0600

    Añadir proyecto de React Native

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import DataScreen from './screens/DataScreen';
+import WeatherScreen from './screens/WeatherScreen';
+import CombinedScreen from './screens/CombinedScreen';
+import ChartScreen from './screens/ChartScreen';
+import PredictionScreen from './screens/PredictionScreen';
+import PredictionClima from './screens/Clima';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Data" component={DataScreen} />
+        <Stack.Screen name="Weather" component={WeatherScreen} />
+        <Stack.Screen name="Combined" component={CombinedScreen} />
+        <Stack.Screen name="Charts" component={ChartScreen} />
+        <Stack.Screen name="Prediction" component={PredictionScreen} />
+        <Stack.Screen name="Clima" component={PredictionClima} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
